93401c48e8216c94cdb69faf735f1622611a34de
Huu Loc <78517332+huynhhuuloc129@users.noreply.github.com>
2021-08-02 15:41:20 +0700

    Add files via upload

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,21 +1,52 @@
-window.onload = mainprogram;
+window.onload = mainprogram();
 function mainprogram() {
-    var qlist = document.getElementsByTagName("pre");
-    for (let i = 0; i < qlist.length; i++) {
-        qlist[i].onclick = showAnswer;
-    }
+    var qlist = document.getElementsByClassName("arrow");
+    var qlist2 = document.getElementsByClassName("question")
+    Array.from(qlist).forEach(element => {
+        element.onclick = showAnswer;
+    });
+    Array.from(qlist2).forEach(element => {
+        element.onclick = showAnswer2;
+    });
 }
-function showAnswer(question) {
-    var ques = question.target;
-    var id = ques.id + "-ans";
+function showAnswer(arrow) {
+    arrow.preventDefault();
+
+    var img = arrow.target;
+    var id = img.parentNode.id + "-ans";
+    var ques = document.getElementById(img.parentNode.id);
     var ans = document.getElementById(id);
 
     if (ans.style.display != "block") {
         ans.style.display = "block";
-        ques.style.fontWeight="1000"
+        ques.style.fontWeight = "1000";
+        img.style.transform = 'rotate(180deg)';
     }
     else {
         ans.style.display = "none";
-        ques.style.fontWeight="500"
+        ques.style.fontWeight = "500";
+        img.style.transform = 'rotate(360deg)';
+    }
+}
+function showAnswer2(question) {
+    question.preventDefault();
+
+    var ques = question.target;
+    var img = ques.firstElementChild;
+    let id = ques.id + "-ans";
+    var ans = document.getElementById(id);
+    if (ans != null) {
+        if (ans.style.display != "block") {
+            ans.style.display = "block";
+            ques.style.fontWeight = "1000";
+            img.style.transform = 'rotate(180deg)';
+
+        }
+        else {
+            ans.style.display = "none";
+            ques.style.fontWeight = "500";
+            img.style.transform = 'rotate(360deg)';
+
+        }
     }
 }
